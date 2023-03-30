7cd02066a75475ba599511ffa565874a395c9dd0
Emily Lubkert <elubkert@gmail.com>
2023-03-30 12:13:54 -0600

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+
+export default function Dashboard() {
+  const [jobDescription, setJobDescription] = useState('');
+
+  const [jobTitle, setJobTitle] = useState('');
+  const [industry, setIndustry] = useState('');
+  const [keyWords, setKeyWords] = useState('');
+  const [tone, setTone] = useState('');
+  const [numWords, setNumWords] = useState('');
+
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsGenerating(true);
+    const res = await fetch('/api/returnJobDescription', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        jobTitle,
+        industry,
+        keyWords,
+        tone,
+        numWords,
+      }),
+    });
+    setIsGenerating(false);
+    const data = await res.json();
+    setJobDescription(data.jobDescription.trim());
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(jobDescription);
+    setIsCopied(true);
+  };
+
+  return (
+    <div className='max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+      <div className='grid gap-y-12 md:grid-cols-2 md:gap-x-12 '>
+        <div className=''>
+          <form onSubmit={e => handleSubmit(e)}>
+            <div className='flex flex-col'>
+              <label className='sr-only' htmlFor='jobTitle'>
+                Job Title
+              </label>
+              <input
+                type='text'
+                className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+                name='jobTitle'
+                placeholder='Job Title'
+                id='jobTitle'
+                value={jobTitle}
+                onChange={(e) => setJobTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor='industry' className='sr-only'>
+                Industry (optional)
+              </label>
+              <input
+                type='text'
+                className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+                name='industry'
+                placeholder='Industry (optional)'
+                id='industry'
+                value={industry}
+                onChange={(e) => setIndustry(e.target.value)}
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor='keywords' className='sr-only'>
+                Keywords for AI (optional)
+              </label>
+              <textarea
+                rows={7}
+                className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+                name='keywords'
+                placeholder='Keywords for AI (optional)'
+                id='keyWords'
+                value={keyWords}
+                onChange={(e) => setKeyWords(e.target.value)}
+              />
+            </div>
+            <div className='flex flex-col'>
+              <label className='sr-only' htmlFor='tone'>
+                Tone
+              </label>
+              <select
+                value={tone}
+                onChange={(e) => setTone(e.target.value)}
+                className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+                name='tone'
+                id='tone'
+              >
+                <option value='default'>Select Tone (optional)</option>
+                <option value='casual'>Casual</option>
+                <option value='friendly'>Friendly</option>
+                <option value='professional'>Professional</option>
+                <option value='formal'>Formal</option>
+              </select>
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor='words' className='sr-only'>
+                Words (optional)
+              </label>
+              <input
+                value={numWords}
+                onChange={(e) => setNumWords(e.target.value)}
+                type='number'
+                className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+                placeholder='Number of Words - default 200 (optional)'
+                name='words'
+                id='words'
+              />
+            </div>
+
+            <button
+              className={`bg-blue-600 w-full hover:bg-blue-700 text-white font-bold mt-6 py-2 px-4 rounded
+                    ${
+                      isGenerating || jobTitle === ''
+                        ? 'cursor-not-allowed opacity-50'
+                        : ''
+                    }`}
+              type='submit'
+              disabled={isGenerating || jobTitle === ''}
+            >
+              {isGenerating ? 'Generating...' : 'Generate Job Description'}
+            </button>
+          </form>
+        </div>
+        <div className=''>
+          <div className='flex flex-col'>
+            <label htmlFor='output' className='sr-only'>
+              Output
+            </label>
+            <textarea
+              rows={
+                jobDescription === ''
+                  ? 7
+                  : jobDescription.split('\\n').length + 12
+              }
+              name='output'
+              onChange={(e) => setJobDescription(e.target.value)}
+              value={jobDescription}
+              disabled={jobDescription === ''}
+              id='output'
+              placeholder='AI Generated Job Description'
+              className='block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900'
+            />
+            <button
+              onClick={handleCopy}
+              className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+              type='submit'
+              disabled={jobDescription === ''}
+            >
+              {isCopied ? 'Copied' : 'Copy to Clipboard'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import Dashboard from '@/components/Dashboard';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AI Job Description Generator</title>
+        <meta name='description' content='AI Job Description Generator' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={'bg-white min-h-screen '}>
+        <div className='flex flex-col items-center justify-center px-4 py-2'>
+          <h1 className='text-4xl md:text-6xl font-bold'>
+            AI Job Description Generator
+            <span className='text-4xl md:text-6xl font-bold text-blue-600'>
+              .
+            </span>
+          </h1>
+          <p className='mt-3 text-2xl'>
+            Create Beautiful
+            <span className='text-2xl font-bold text-blue-600'>
+              {' '}
+              Job Descriptions{' '}
+            </span>
+            in Seconds
+          </p>
+        </div>
+        <Dashboard />
+      </main>
+    </>
+  );
+}
